7dfd9cf9f6c77fcc5e560908f0a128bec8cac826
SatrioBayu <sbayu875@gmail.com>
2023-05-10 10:21:54 +0700

    feat: add swagger-ui-express package and serve the API docs

--- app/router.js
+++ app/router.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { authenticationController } = require("./controller");
+const swaggerDocument = require("../docs/openapi.json");
+const swaggerUI = require("swagger-ui-express");
 
 router.get("/", (req, res) => {
   res.send({
     message: "Halaman home",
   });
 });
+
+// API Docs Swagger
+router.use("/api-docs/json", (req, res) => {
+  res.json(swaggerDocument);
+});
+router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // User
 router.post("/register", authenticationController.handleRegister);
