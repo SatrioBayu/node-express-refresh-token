7eebc63e24f79068bf50c21ac5d182ec17589502
SatrioBayu <sbayu875@gmail.com>
2023-05-11 10:53:22 +0700

    feat: add cron.js and define the job and schedule

---
+++ app/cron.js
@@ -0,0 +1,17 @@
+const CronJob = require("cron").CronJob;
+const { InvalidToken } = require("../app/models");
+
+const clearInvalidTokenTableJob = new CronJob("0 0 * * * *", async () => {
+  // your code to clear the table goes here
+  try {
+    await InvalidToken.destroy({
+      where: {},
+      truncate: true,
+    });
+    console.log("Table cleared");
+  } catch (error) {
+    console.error("Error clearing table:", error);
+  }
+});
+
+module.exports = { clearInvalidTokenTableJob };
