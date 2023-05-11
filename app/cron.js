6931d532652cf180e399e4d4771bffabff7024a6
SatrioBayu <sbayu875@gmail.com>
2023-05-11 11:42:34 +0700

    feat: change schedule in cron to every hour

--- app/cron.js
+++ app/cron.js
@@ -1,7 +1,7 @@
 const CronJob = require("cron").CronJob;
 const { InvalidToken } = require("../app/models");
 
-const clearInvalidTokenTableJob = new CronJob("0 * * * * *", async () => {
+const clearInvalidTokenTableJob = new CronJob("0 0 * * * *", async () => {
   // your code to clear the table goes here
   try {
     await InvalidToken.destroy({
