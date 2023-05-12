6a7d53da256333802d2c3be62e02b5cb9d923b57
SatrioBayu <sbayu875@gmail.com>
2023-05-12 10:01:14 +0700

    feat: add index.js in errors for exporting

---
+++ app/errors/index.js
@@ -0,0 +1,5 @@
+const authErr = require("./AuthError");
+const internalErr = require("./InternalServerError");
+const usernameErr = require("./UsernameError");
+
+module.exports = { authErr, internalErr, usernameErr };
