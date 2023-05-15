ea50576a70bc682a5eadae1f20098f0563a2a83e
SatrioBayu <sbayu875@gmail.com>
2023-05-15 11:23:49 +0700

    feat: add unit test for handle logout

--- app/controller/authenticationController.test.js
+++ app/controller/authenticationController.test.js
@@ -424,6 +424,159 @@ describe("authenticationController", () => {
     });
   });
 
+  describe("handleLogout", () => {
+    it("should return 400 if there's no username or password in body", async () => {
+      const mockReq = {
+        body: {
+          username: "Jabran",
+        },
+      };
+
+      // Validate the request using registerValidator
+      const errors = validationResult(mockReq);
+      const errorArray = errors.array().map((err) => {
+        return { code: err.msg, message: err.msg };
+      });
+
+      // Check if there are validation errors
+      if (!errors.isEmpty()) {
+        const mockRes = {
+          status: jest.fn().mockReturnThis(),
+          send: jest.fn().mockReturnThis(),
+        };
+        await authenticationController.handleLogout(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(400);
+        expect(mockRes.send).toHaveBeenCalledWith({
+          errors: errorArray,
+        });
+      }
+    });
+    it("should return 401 if the token is invalid, malformed or expired", async () => {
+      const mockReq = {
+        body: {
+          username: "Jisoo",
+          token:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFmNWMwZjI3LWJhZTktNDJjNC1hMWU0LTBiN2I1MTZmNDBjYyIsImlhdCI6MTY4Mzg4MjYzNywiZXhwIjoxNjgzODg0NDM3fQ.sJxtLTu1MLwYEhwuVbJ2CfLYIjSAm5qqwwqX7Wif0xAss",
+        },
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      await authenticationController.handleLogout(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(401);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-006",
+            message: "Token tidak valid / diubah secara sengaja",
+          },
+        ],
+      });
+    });
+    it("should return 401 if the username in body and user fetch from id didn't match", async () => {
+      const token = jwt.sign({ id: "2572b205-1439-420e-9f8f-3958bfda88cf" }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
+
+      const mockReq = {
+        body: {
+          username: "Jisoo",
+          token,
+        },
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      authenticationController.handleLogout(mockReq, mockRes, () => {
+        expect(mockRes.status).toHaveBeenCalledWith(401);
+        expect(mockRes.send).toHaveBeenCalledWith({
+          errors: [
+            {
+              code: "E-007",
+              message: "Token tidak valid untuk user ini",
+            },
+          ],
+        });
+        done();
+      });
+    });
+    it("should return 404 if the id inside token isn't match any Users", async () => {
+      const token = jwt.sign({ id: "8d04658b-cf4e-46df-8d6f-1aa4a85f0c36" }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
+
+      const mockReq = {
+        body: {
+          username: "Jisoo",
+          token,
+        },
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      authenticationController.handleLogout(mockReq, mockRes, () => {
+        expect(mockRes.status).toHaveBeenCalledWith(404);
+        expect(mockRes.send).toHaveBeenCalledWith({
+          errors: [
+            {
+              code: "E-011",
+              message: "User tidak ditemukan",
+            },
+          ],
+        });
+        done();
+      });
+    });
+    it("should return 500 if there's error in catch block", async () => {
+      const mockReq = {};
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      await authenticationController.handleLogout(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(500);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-013",
+            message: expect.any(String),
+          },
+        ],
+      });
+    });
+    it("should return 200 if the username and token valid also match each other", async () => {
+      const token = jwt.sign({ id: "2572b205-1439-420e-9f8f-3958bfda88cf" }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
+
+      const mockReq = {
+        body: {
+          username: "Lisa",
+          token,
+        },
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      authenticationController.handleLogout(mockReq, mockRes, () => {
+        expect(mockRes.status).toHaveBeenCalledWith(200);
+        expect(mockRes.send).toHaveBeenCalledWith({
+          message: "Berhasil logout",
+        });
+        done();
+      });
+    });
+  });
+
   describe("handleWhoAmI", () => {
     it("should return 500 if there's error in try catch block", async () => {
       const mockReq = {
