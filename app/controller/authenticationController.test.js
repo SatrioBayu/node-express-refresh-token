d5770448c74a8e7c461eac3d86b05cd706dd0c23
SatrioBayu <sbayu875@gmail.com>
2023-05-15 10:36:22 +0700

    feat: add unit test for handle auth

--- app/controller/authenticationController.test.js
+++ app/controller/authenticationController.test.js
@@ -1,6 +1,7 @@
 const authenticationController = require("./authenticationController");
 const { User, InvalidToken } = require("../models");
 const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 afterAll(async () => {
   await User.destroy({
@@ -11,6 +12,160 @@ afterAll(async () => {
 });
 
 describe("authenticationController", () => {
+  describe("handleAuth", () => {
+    it("should return 401 if the token not provided", async () => {
+      const mockReq = {
+        headers: {
+          authorization: null,
+        },
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      const next = jest.fn();
+
+      await authenticationController.handleAuth(mockReq, mockRes, next);
+
+      expect(mockRes.status).toHaveBeenCalledWith(401);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-004",
+            message: "Token tidak disediakan",
+          },
+        ],
+      });
+    });
+    it("should return 401 if the token is invalid, malformed or expired", async () => {
+      const mockReq = {
+        headers: {
+          authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFmNWMwZjI3LWJhZTktNDJjNC1hMWU0LTBiN2I1MTZmNDBjYyIsImlhdCI6MTY4Mzg4MjYzNywiZXhwIjoxNjgzODg0NDM3fQ.sJxtLTu1MLwYEhwuVbJ2CfLYIjSAm5qqwwqX7Wif0xAss",
+        },
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      const next = jest.fn();
+
+      await authenticationController.handleAuth(mockReq, mockRes, next);
+
+      expect(mockRes.status).toHaveBeenCalledWith(401);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-006",
+            message: "Token tidak valid / diubah secara sengaja",
+          },
+        ],
+      });
+    });
+    it("should return 401 if the server can't find who the token belongs to", async () => {
+      const token = jwt.sign({ id: "8d04658b-cf4e-46df-8d6f-1aa4a85f0c36" }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
+      const mockReq = {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+      const next = jest.fn();
+
+      authenticationController.handleAuth(mockReq, mockRes, next, () => {
+        expect(mockRes.status).toHaveBeenCalledWith(401);
+        expect(mockRes.send).toHaveBeenCalledWith({
+          errors: [
+            {
+              code: "E-008",
+              message: "Tidak diotorisasi",
+            },
+          ],
+        });
+        done();
+      });
+    });
+    it("should return 403 if the token is blocked", async () => {
+      const mockReq = {
+        headers: {
+          authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFmNWMwZjI3LWJhZTktNDJjNC1hMWU0LTBiN2I1MTZmNDBjYyIsImlhdCI6MTY4Mzc4ODA4NCwiZXhwIjoxNjgzNzg5ODg0fQ.VZJ6Fc9cfSsXncW1odYEf6tDsEJkxsr52GSKb4F666M",
+        },
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      const next = jest.fn();
+
+      await authenticationController.handleAuth(mockReq, mockRes, next);
+
+      expect(mockRes.status).toHaveBeenCalledWith(403);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-005",
+            message: "Token telah diblokir",
+          },
+        ],
+      });
+    });
+    it("should return 500 if there's error in try catch block", async () => {
+      const mockReq = {
+        body: {},
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      const next = jest.fn();
+
+      await authenticationController.handleAuth(mockReq, mockRes, next);
+
+      expect(mockRes.status).toHaveBeenCalledWith(500);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-013",
+            message: expect.any(String),
+          },
+        ],
+      });
+    });
+    it("should call next if the token is valid", async () => {
+      const token = jwt.sign({ id: "2572b205-1439-420e-9f8f-3958bfda88cf" }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
+
+      const mockReq = {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis,
+        send: jest.fn().mockReturnThis,
+      };
+
+      const next = jest.fn();
+
+      authenticationController.handleAuth(mockReq, mockRes, next, () => {
+        expect(next).toHaveBeenCalled();
+        done();
+      });
+    });
+  });
+
   describe("handleRegister", () => {
     it("should return 400 if the username already registered", async () => {
       const mockReq = {
