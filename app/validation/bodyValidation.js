01a8d6a815696ce012f33ceac8df04f92a7e0705
SatrioBayu <sbayu875@gmail.com>
2023-05-15 15:41:28 +0700

    feat: add body validation for update username, and password

--- app/validation/bodyValidation.js
+++ app/validation/bodyValidation.js
@@ -12,11 +12,34 @@ module.exports = {
       code: "E-002",
       message: "Password tidak boleh kosong",
     }),
+    body("password").isLength(6).withMessage({
+      code: "E-003",
+      message: "Password minimal 6 karakter",
+    }),
+  ],
+  updateUsernameValidate: [usernameValidator],
+  updatePasswordValidate: [
+    body("oldPassword").notEmpty().withMessage({
+      code: "E-002",
+      message: "Password lama tidak boleh kosong",
+    }),
+    body("oldPassword").isLength(6).withMessage({
+      code: "E-003",
+      message: "Password lama minimal 6 karakter",
+    }),
+    body("newPassword").notEmpty().withMessage({
+      code: "E-002",
+      message: "Password baru tidak boleh kosong",
+    }),
+    body("newPassword").isLength(6).withMessage({
+      code: "E-003",
+      message: "Password baru minimal 6 karakter",
+    }),
   ],
   logoutValidate: [
     usernameValidator,
     body("token").notEmpty().withMessage({
-      code: "E-003",
+      code: "E-004",
       message: "Token tidak boleh kosong",
     }),
   ],
